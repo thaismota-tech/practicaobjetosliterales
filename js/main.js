4f1ac10a4ccbaed54c30504788db9b3e0c216ebc
thaismota-tech <thaissantosdamota@gmail.com>
2026-04-24 21:24:41 +0200

    He finalizado el ejercicio 1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// 1. Crear un objeto Persona
+let objPersona = {
+  nombre: "Joanie",
+  edad: "25",
+  ciudad: "Paris",
+}
+console.log(objPersona.nombre);
+console.log(objPersona.edad);
+console.log(objPersona.ciudad);
